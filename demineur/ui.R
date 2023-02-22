abb31a72b80d2b63455b09865dffa40bf6077179
sarahmatoub <sarah.matoub@etu.umontpellier.fr>
2023-02-22 15:47:48 +0100

    upload file ui.R

--- demineur/ui.R
+++ demineur/ui.R
@@ -1,11 +1,19 @@
-library(shiny)
-
-ui <- fluidPage(
-  titlePanel("Démineur"),
-  sidebarPanel(
-    selectInput("select", label =h3("Niveau") , choices = list("Null", "facile", "Moyen","Difficile")
-                )
-  )
+# Define UI for miles per gallon app ----
+ui <- pageWithSidebar(
   
+  # App title ----
+  titlePanel("Jeu du démineur"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(
+    
+    # Input: Selector for variable to plot against mpg ----
+    selectInput("select", label = h3("Choisir un niveau"),
+                choices = list(" ", "Facile", "Moyen", "Difficile")
+                  ),
+    
+  ),
+  
+  # Main panel for displaying outputs ----
+  mainPanel()
 )
-
