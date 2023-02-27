6f2114ad6c03651486c5adf883ed4498b11328cd
Thiziriinfo <103370452+Thiziriinfo@users.noreply.github.com>
2023-02-27 17:46:18 +0100

    update

--- demineur/ui.R
+++ demineur/ui.R
@@ -2,28 +2,37 @@ library(shiny)
 
 ui <- fluidPage(
   
-  titlePanel("Jeu du démineur"),
+  titlePanel("Démineur"),
+  textOutput("Bienvenue dansle jeu du Démineur , vous avez ci-dessous deux option : 
+             soit vous jouez par sélection du niveau  que vous désirez
+             soit par personnalisation du jeu selons vos gouts 
+             A vous de jouer!!! "),
   
   sidebarLayout(
     sidebarPanel(
       selectInput("Niveau", "Choisir un niveau:",
                   choices=c("Facile", "Moyen", "Difficile"),
                   selected = "Facile"),
+      numericInput("Personnalisé","Personnalisé",value=0 , min =0),
+      numericInput("n_rows", "Nombre de lignes ", value = 10, min = 1),
+      numericInput("n_cols", "Nombre de colonnes", value = 10, min = 1),
+      numericInput("n_mines", "Nombre de mines", value = 10, min = 1),
+      actionButton("nouvelle partie ", "jouer "))
       
     ),
     
-    mainPanel(
-      fluidRow( 
-        column(width = 12,
-               h4("Cliquer sur la case pour connaître sa valeur"),
-               h4("Cliquer sur la case pour mettre un drapeau"))
-      ),
+    #mainPanel(
+      ###########fluidRow( 
+       ########## column(width = 12,
+        #########       h4("Cliquer sur la case pour connaître sa valeur"),
+         ########      h4("Cliquer sur la case pour mettre un drapeau"))
+     ####### ),
       
-      fluidRow(
-        column(width = 12,
-               uiOutput("Grille_de_jeu"))
-      )
-    )
-  )
-)
+      ######fluidRow(
+        #####column(width = 12,
+              #### uiOutput("Grille_de_jeu"))
+     ### )
+    ##)##
+  #)
+
 
