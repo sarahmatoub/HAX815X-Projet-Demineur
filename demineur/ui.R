fc651d4cca58376d207ed8d43848296515ffe01a
sarahmatoub <sarah.matoub@etu.umontpellier.fr>
2023-02-28 10:52:14 +0100

    fetch

--- demineur/ui.R
+++ demineur/ui.R
@@ -2,7 +2,11 @@ library(shiny)
 
 ui <- fluidPage(
   
-  titlePanel("Jeu du démineur"),
+  titlePanel("Démineur"),
+  textOutput("Bienvenue dans le jeu du Démineur , vous avez ci-dessous deux options : 
+             soit vous jouez par sélection du niveau  que vous désirez
+             soit par personnalisation du jeu selons vos souhaits 
+             A vous de jouer!!! "),
   
   sidebarLayout(
     sidebarPanel(
@@ -10,32 +14,26 @@ ui <- fluidPage(
                   choices=c("Facile", "Moyen", "Difficile"),
                   selected = "Facile"),
       
+      numericInput("n_rows", "Nombre de lignes ", value = 10, min = 1),
+      numericInput("n_cols", "Nombre de colonnes", value = 10, min = 1),
+      numericInput("n_mines", "Nombre de mines", value = 10, min = 1),
+      actionButton("nouvelle partie ", "jouer "))
+    
+  ),
+  
+  mainPanel(
+    fluidRow( 
+      column(width = 12,
+             h4("Cliquer sur la case pour connaître sa valeur"),
+             h4("Cliquer sur la case pour mettre un drapeau"))
     ),
     
-    mainPanel(
-      fluidRow( 
-        column(width = 12,
-               h4("Cliquer sur la case pour connaître sa valeur"),
-               h4("Cliquer sur la case pour mettre un drapeau"))
-      ),
-      
-      fluidRow(
-        column(width = 12,
-               uiOutput("Grille_de_jeu"))
-      )
+    fluidRow(
+      column(width = 12,
+             uiOutput("Grille_de_jeu"))
     )
   )
 )
-
-
-
-
-server <- function(input, output, session) {
-  
-}
-
-shinyApp(ui, server)
-
 
 
 
